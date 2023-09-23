e8d7a4b5dc70c02b30bd1b2700b85bc082e4ac69
saifrazaa <razasaif171@gmail.com>
2023-09-23 12:17:22 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <></>
+    </NavigationContainer>
+  );
+}
+
+export default App;
